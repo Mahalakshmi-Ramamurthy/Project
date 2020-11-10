4d210fbcf9153d2b9c562294cccf8d835b3a812c
Mahalakshmi Ramamurthy <mahalakshmi.mop@gmail.com>
2020-11-10 11:26:55 +0000

    commit1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,43 @@
+	var oView
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.cam.camera.controller.View1", {
+		onInit: function () {
+	 oView = this.getView();
+		},
+		onCapture: function() {
+		
+        navigator.camera.getPicture(this.onSuccess, this.onFail, {
+            quality: 75,
+            targetWidth: 300,
+            targetHeight: 300,
+            sourceType: navigator.camera.PictureSourceType.CAMERA,
+            destinationType: navigator.camera.DestinationType.FILE_URI
+        });
+    },
+    onSelect: function() {
+    	
+        navigator.camera.getPicture(this.onSuccess, this.onFail, {
+            quality: 75,
+            targetWidth: 300,
+            targetHeight: 300,
+            sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY,
+            destinationType: navigator.camera.DestinationType.FILE_URI
+        });
+    },
+    onSuccess: function(imageData) {
+    	 alert(imageData);
+        var imageId = oView.byId("myImage");
+        imageId.setSrc(imageData);
+       
+
+    },
+    onFail: function(message) {
+        alert("Failed because: " + message);
+    }
+
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/cam/camera/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
